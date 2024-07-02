6e6661fcb29197b0e4923030268dd8155b429342
Zac M-W <z.millerwaugh@auckland.ac.nz>
2024-07-02 17:26:19 +1200

    Super duper mock UI

--- src/routes/quick/+page.svelte
+++ src/routes/quick/+page.svelte
@@ -1,29 +1,63 @@
 <div class="grid grid-cols-3 min-h-screen w-full gap-8 p-8">
   <!-- sidebar -->
   <div class="col-span-1">
-    <div class="sidebar">
-      <div class="sidebar-header">
-        <h1 class="text-2xl font-bold">Quick</h1>
-      </div>
-      <div class="sidebar-content">
-        <ul class="nav">
-          <li class="nav-item">
-            <a href="/quick" class="nav-link">Home</a>
-          </li>
-          <li class="nav-item">
-            <a href="/detailed" class="nav-link">Detailed</a>
-          </li>
-          <li class="nav-item">
-            <a href="/comprehensive" class="nav-link">Comprehensive</a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <div class="sidebar"></div>
   </div>
 
   <div class="col-span-2">
-    <main class="rounded-box shadow-xl bg-base-100 w-full h-full p-10">
-      <h2 class="font-bold text-4xl">Test</h2>
+    <main class="rounded-box shadow-xl bg-base-100 w-full h-full py-14 px-14">
+      <h2 class="font-bold text-4xl">Introduction</h2>
+      <p class="text-lg">
+        The Quick review tool will give you a broad overview of where you might
+        need to focus to improve your course. Text going here should explain
+        more about the tool and have good word usage to make word look good
+        here. When you’re ready to get started, click Get Started.
+      </p>
+
+      <div class="stack-container flex justify-center mt-10">
+        <div class="stack mx-auto">
+          <div class="card bg-base-200 max-w-sm text-base-content shadow-md">
+            <div class="card-body">
+              <h2 class="card-title">
+                The online learning environment includes administrative,
+                technical and learning information and support.
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                faucibus posuere nulla eu eleifend. Nullam et bibendum nibh.
+                Nulla venenatis malesuada tortor, quis tincidunt dui viverra sit
+                amet. Morbi vel dapibus sapien. Class aptent taciti sociosqu ad.
+              </p>
+              <div class="card-actions justify-end">
+                <div class="rating">
+                  <input type="radio" name="rating-1" class="mask mask-star" />
+                  <input
+                    type="radio"
+                    name="rating-1"
+                    class="mask mask-star"
+                    checked="checked"
+                  />
+                  <input type="radio" name="rating-1" class="mask mask-star" />
+                  <input type="radio" name="rating-1" class="mask mask-star" />
+                  <input type="radio" name="rating-1" class="mask mask-star" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card bg-base-200 max-w-sm text-base-content shadow">
+            <div class="card-body">
+              <h2 class="card-title">Notification 2</h2>
+              <p>You have 3 unread messages. Tap here to see.</p>
+            </div>
+          </div>
+          <div class="card bg-base-200 max-w-sm text-base-content shadow-sm">
+            <div class="card-body">
+              <h2 class="card-title">Notification 3</h2>
+              <p>You have 3 unread messages. Tap here to see.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   </div>
 </div>
